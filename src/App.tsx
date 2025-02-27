b3c8e236bcec734a3799490b853edb66bc69ebdf
Carter Walsh <cartwalsh@gmail.com>
2025-02-26 21:29:35 -0600

    made functions for setting the type, abilities, and attacks on the cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { PokemonCard, CardImages, Set, Type, Subtype, Ability, Attack, TypeOnCard, SubtypeOnCard, AbilityOnCard,
-    AttackOnCard, WeaknessOnCard, ResistanceOnCard, ImagesOnCard, PokedexNumber} from '/utils/models.ts'
+import {
+    PokemonCard, CardImages, Set, Type, Subtype, Ability, Attack, TypeOnCard, SubtypeOnCard, AbilityOnCard,
+    AttackOnCard, WeaknessOnCard, ResistanceOnCard, ImagesOnCard, PokedexNumber
+} from '/utils/models.ts'
 
 import {Box, Center, Flex, Heading, Input} from "@chakra-ui/react";
 
 
 interface TCGCard {
-    abilities: { name: string; text: string }[];
+    cardID: string
+    abilities: Ability[];
     attacks: { name: string; cost: string[]; convertedEnergyCost: number; damage: string; text: string; }[];
     convertedRetreatCost: number;
     evolvesFrom?: string;
@@ -25,10 +28,9 @@ interface TCGCard {
     set: { id: string; setName: string; series: string; };
     stage?: string[];
     supertype: string;
-    types: string[];
+    types: Type[];
     weaknesses: { type: string; value: number }[]; // Value should be a number
 }
-
 
 
 function App() {
@@ -37,15 +39,15 @@ function App() {
 
     const [cardCollection, setCardCollection] = useState<TCGCard[]>([])
     const [setList, setSetList] = useState([]);
-    const [typeList, setTypeList] = useState([]);
-    const [typeOnCardList, setTypeOnCardList] = useState([]);
-    const [abilityList, setAbilityList] = useState([]);
-    const [abilityOnCardList, setAbilityOnCardList] = useState([]);
-    const [attackList, setAttackList] = useState([]);
-    const [attackOnCardList, setAttackOnCardList] = useState([]);
-    const [weaknessList, setWeaknessList] = useState([]);
-    const [resistanceList, setResistanceList] = useState([]);
-    const [pokedexNumberList, setPokedexNumberList] = useState([]);
+    const [typeList, setTypeList] = useState<Type[]>([]);
+    const [typeOnCardList, setTypeOnCardList] = useState<TypeOnCard[]>([]);
+    const [abilityList, setAbilityList] = useState<Ability[]>([]);
+    const [abilityOnCardList, setAbilityOnCardList] = useState<AbilityOnCard[]>([]);
+    const [attackList, setAttackList] = useState<Attack[]>([]);
+    const [attackOnCardList, setAttackOnCardList] = useState<AttackOnCard[]>([]);
+    const [weaknessList, setWeaknessList] = useState<WeaknessOnCard[]>([]);
+    const [resistanceList, setResistanceList] = useState<ResistanceOnCard[]>([]);
+    const [pokedexNumberList, setPokedexNumberList] = useState<PokedexNumber[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
@@ -89,13 +91,14 @@ function App() {
             .then(data => {
                 // console.log(data)
 
-                const deck : TCGCard[] = []
+                const deck: TCGCard[] = []
                 for (let i = 0; i < data.length; i++) {
 
                     const card = data[i]
                     // console.log(card)
 
                     const cardobj: TCGCard = {
+                        cardID: card.cardID,
                         abilities: card.abilities || [], // Ensure it's an array
                         attacks: card.attacks || [], // Ensure it's an array
                         convertedRetreatCost: card.convertedRetreatCost ?? 0, // Default to 0 if missing
@@ -132,62 +135,117 @@ function App() {
 
                     deck.push(cardobj);
 
-                    // console.log(deck)
-                    // console.log(cardobj)
                 }
                 setCardCollection(deck)
             })
         setLoading(false);
     }, [setPokedexNumberList])
 
-    useEffect(() => {
 
-        const cID = cardCollection[5164]
+    const setTypes = (card: TCGCard) => {
 
-        console.log('cID', cID)
+        const types: Type[] = []
 
-        console.log(typeOnCardList[0])
+        const pkType: TypeOnCard = typeOnCardList.filter((x: TypeOnCard) => x.tocID === card.cardID)
 
-        const pkType = typeOnCardList.find((x: TypeOnCard) => x.cardID !== cID)
+        const typeobj = pkType[0]
 
-        console.log('pkType', pkType)
+        if (typeobj === undefined || typeobj === null) return types;
 
-        // for (let i=0; i<cardCollection.length; i++) {
-        //     let card = cardCollection[i];
-        //
-        //
-        //
-        // }
+        const type: Type = typeList.filter((x: Type) => x.typeID === pkType[0].typeID)
 
-    }, [cardCollection]);
+        if (type.length > 1) for (const t of type) types.push(t)
+        else types.push(type[0])
 
-    if (loading) return (<Center>Loading...</Center>)
+        return types;
+    }
+    const setAbilities = (card: TCGCard) => {
 
-    return (
-        <Flex bg={'red.100'} justify={'center'} align={'center'} fontWeight={'semibold'} direction={'column'}>
-            {/*  Search Filters  */}
-            <Flex bg={'blue.200'} justify={'space-around'} align={'center'} direction={'column'} w={'90%'}>
-                <Flex bg={'blue.100'} justify={'space-around'} align={'center'} direction={'row'} w={'500px'}>
-                    <Box>Search by Name</Box>
-                    <Input bg={'white'} w={'60%'}/>
-                </Flex>
-            </Flex>
+        const abilities: Ability[] = []
 
-            {/*  Card Gallery  */}
-            <Flex direction={'row'} justify={'space-around'} align={'center'} bg={'green.400'} p={5} wrap={'wrap'} w={'90%'}>
-                <Flex justify={'center'} align={'center'} wrap={'wrap'}>
-                    {
-                        cardCollection.map((card: TCGCard, index: number) => (
-                            <DisplayCard key={index} card={card} />
-                        ))
-                    }
-            </Flex>
+        const pkAbility: AbilityOnCard = abilityOnCardList.filter((x: AbilityOnCard) => x.cardID === card.cardID)
+
+        const abilityobj = pkAbility[0]
+
+        if (abilityobj === undefined || abilityobj === null) return abilities;
+
+        const ability: Ability = abilityList.filter((x: Ability) => x.abilityID === pkAbility[0].abilityID)
+
+        if (pkAbility.length === 0) return abilities;
+
+        if (ability.length > 1) for (const t of ability) abilities.push(t)
+        else abilities.push(ability[0])
+
+        return abilities;
+    }
+
+    const setAttacks = (card: TCGCard) => {
+
+        let attacks: Attack[] = []
+
+        const pkAttack: AttackOnCard = attackOnCardList.filter((x: AttackOnCard) => x.cardID === card.id)
+        // console.log(pkAttack)
+        const attackobj = pkAttack[0]
+
+        if (attackobj === undefined || attackobj === null) return attacks;
+
+        attacks = []
+
+        const attack: Attack = attackList.filter((x: Attack) => x.attackID === pkAttack[0].attackID)
+
+        if (pkAttack.length === 0) return attacks;
+
+        if (attack.length > 1) for (const t of attack) attacks.push(t)
+        else attacks.push(attack[0])
+
+        return attacks;
+    }
+
+
+
+
+useEffect(() => {
+
+    for (let i = 0; i < cardCollection.length; i++) {
+        cardCollection[i].types = setTypes(cardCollection[i])
+        // console.log(cardCollection[i].types)
+        cardCollection[i].abilities = setAbilities(cardCollection[i])
+        // console.log(cardCollection[i].abilities)
+        cardCollection[i].attacks = setAttacks(cardCollection[i])
+        // console.log(cardCollection[i].attacks)
+
+    }
+
+}, [cardCollection]);
+
+if (loading) return (<Center>Loading...</Center>)
+
+return (
+    <Flex bg={'red.100'} justify={'center'} align={'center'} fontWeight={'semibold'} direction={'column'}>
+        {/*  Search Filters  */}
+        <Flex bg={'blue.200'} justify={'space-around'} align={'center'} direction={'column'} w={'90%'}>
+            <Flex bg={'blue.100'} justify={'space-around'} align={'center'} direction={'row'} w={'500px'}>
+                <Box>Search by Name</Box>
+                <Input bg={'white'} w={'60%'}/>
             </Flex>
         </Flex>
-    )
+
+        {/*  Card Gallery  */}
+        <Flex direction={'row'} justify={'space-around'} align={'center'} bg={'green.400'} p={5} wrap={'wrap'}
+              w={'90%'}>
+            <Flex justify={'center'} align={'center'} wrap={'wrap'}>
+                {
+                    cardCollection.map((card: TCGCard, index: number) => (
+                        <DisplayCard key={index} card={card}/>
+                    ))
+                }
+            </Flex>
+        </Flex>
+    </Flex>
+)
 }
 
-function DisplayCard({ card }: { card: TCGCard }) {
+function DisplayCard({card}: { card: TCGCard }) {
     return (
         <Flex p={3} m={2} bg={'gray.400'}>
             {card.name}
